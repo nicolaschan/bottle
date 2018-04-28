c220f6b61280bb3436307a7b4beb20fc5a56e078
Nicolas Chan <nicolas@nicolaschan.com>
2018-04-28 03:18:54 -0700

    Begin client

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 extern crate pancurses;
+extern crate websocket;
 
 use pancurses::{initscr, endwin};
 
+mod client;
+
 fn main() {
+  let mut socket = client::Socket::new();
+  socket.send_message("Hello from the client").unwrap();
+
   let window = initscr();
   window.printw("Bottle");
   window.refresh();
