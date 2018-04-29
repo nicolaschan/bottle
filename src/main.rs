3a000b894517103d89488773734d87ff3b1bfe31
noloerino <jhshi07@gmail.com>
2018-04-29 16:20:41 -0700

    Moved main onto main thread

--- src/main.rs
+++ src/main.rs
@@ -10,29 +10,28 @@ mod chat_window;
 fn main() {
   let (tx, rx) = channel();
 
-  thread::spawn(move || {
-    let window = initscr();
-    let mut chat = chat_window::ChatWindow::new(&window);
-    start_color();
-    noecho();
-    window.printw("BOTTLE ALPHA v0.1.0");
-    window.keypad(true);
-    window.mvaddstr(1, 0, "Press enter to go to the test chat mode thing");
-    pancurses::use_default_colors();
-    window.refresh();
+  let window = initscr();
+  start_color();
+  noecho();
+  window.printw("BOTTLE ALPHA v0.1.0");
+  window.keypad(true);
+  window.mvaddstr(1, 0, "Press enter to go to the test chat mode thing");
+  pancurses::use_default_colors();
+  window.refresh();
+  loop {
     match window.getch() {
-      Some(Input::KeyF1) => {},
+      Some(Input::KeyF1) => { break; },
       Some(Input::KeyEnter) | Some(Input::Character('\n')) => {
+        let mut chat = chat_window::ChatWindow::new(&window);
         window.nodelay(true);
         window.mv(window.get_max_y() / 2, window.get_max_x() / 2 - 5);
         window.printw("loading...");
         window.refresh();
         chat.run(rx, tx);
+        break;
       },
       _ => ()
     }
-    endwin();
-  });
-
-  loop {}
+  }
+  endwin();
 }
